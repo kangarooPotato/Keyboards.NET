032059b9a166f402370ec3cc809f932257a4abf3
Hyunjun Lee <90293400+kangarooPotato@users.noreply.github.com>
2021-10-18 23:52:49 -0400

    [study] Week 4 ASP.NET
    Followed Week3 Class( Recording_14 )
    Installing the dotnet-ef tools

--- Startup.cs
+++ Startup.cs
@@ -14,26 +14,36 @@ namespace Keyboards
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsDevelopment())
+            {         
+                    services.AddDbContext<KeyboardContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("KeyboardContext")));
+            }
+            else
+            {
+                    services.AddDbContext<KeyboardContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("KeyboardContext")));
+            }
             services.AddRazorPages();
 
-            services.AddDbContext<KeyboardContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("KeyboardContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -49,7 +59,7 @@ namespace Keyboards
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
